c5762465b74faf9ca486ee5c1c406b02742613e8
Martin <marti@LAPTOP-36O6IBO3>
2022-06-21 20:45:28 -0300

    se agrego parametros a "IngresoUsuario"

--- src/test/java/Edit/Proyect123mvd/Abercrombie1.java
+++ src/test/java/Edit/Proyect123mvd/Abercrombie1.java
@@ -1,29 +1,52 @@
 package Edit.Proyect123mvd;
 
-import org.junit.Test;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Abercrombie1 {
 	String url = "https://brooksfield.com.uy/";
 	String driverPath = "..\\Proyect123mvd\\Drivers\\chromedriver.exe";
-	@Test
-	public void registrarUsuario(){
+	WebDriver driver;
+	@BeforeSuite
+	public void abrirPagina() {
 		System.setProperty("webdriver.chrome.driver", driverPath);
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("https://brooksfield.com.uy/");
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		
+	}
+	// ingreso de usuario registrado
+	@Test(description= "CP01 - Ingresar Usuario Existente", priority=1)
+	public void IngresoUsuario(){
 		driver.findElement(By.id("headerProfile")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("signInEmail")));
 		driver.findElement(By.id("signInEmail")).sendKeys("autom3775@gmail.com");
 		driver.findElement(By.cssSelector("#signInPassword")).sendKeys("mat@3775");
-		driver.findElement(By.cssSelector("#signInPassword")).sendKeys("mat@3775");
+		driver.findElement(By.cssSelector("#signInButton")).click();
+		
+		
 		
 		
 	}
 	
+	
+	/*
+	@AfterSuite
+	public void cerrarPagina() {
+		driver.close();
+	}
+	*/
 }
