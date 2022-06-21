4b7a8565852a65496cc9b96073b7a029d28944ba
Martin <marti@LAPTOP-36O6IBO3>
2022-06-21 14:04:25 -0300

    Commit inicial

---
+++ src/test/java/Edit/Proyect123mvd/Abercrombie1.java
@@ -0,0 +1,28 @@
+package Edit.Proyect123mvd;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Abercrombie1 {
+	String url = "https://brooksfield.com.uy/";
+	String driverPath = "..\\Proyect123mvd\\Drivers\\chromedriver.exe";
+	@Test
+	public void registrarUsuario(){
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://brooksfield.com.uy/");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("headerProfile")).click();
+		driver.findElement(By.id("signInEmail")).sendKeys("autom3775@gmail.com");
+		driver.findElement(By.cssSelector("#signInPassword")).sendKeys("mat@3775");
+		
+		
+	}
+	
+}
