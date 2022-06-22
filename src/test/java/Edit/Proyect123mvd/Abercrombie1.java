c2206a7b947756b085dc3b92593c6b87d0fabf37
Martin <marti@LAPTOP-36O6IBO3>
2022-06-22 11:47:24 -0300

    Registro usuario correcto

--- src/test/java/Edit/Proyect123mvd/Abercrombie1.java
+++ src/test/java/Edit/Proyect123mvd/Abercrombie1.java
@@ -27,17 +27,37 @@ public class Abercrombie1 {
 		driver.manage().window().maximize();
 		
 	}
+	@Test
+	public void RegistroUsuarioCorrecto() {
+		driver.findElement(By.id("headerProfile")).click();
+		driver.findElement(By.cssSelector("#changeToRegisterButton")).click();
+		driver.findElement(By.id("registerName")).click();
+		driver.findElement(By.id("registerName")).sendKeys("Maximo");
+		driver.findElement(By.id("registerSurname")).sendKeys("Lopez");
+		driver.findElement(By.id("registerEmail")).sendKeys("autom3775@gmail.com");
+		driver.findElement(By.cssSelector("#registerPassword")).sendKeys("mat@3775");
+		driver.findElement(By.cssSelector("#registerPasswordRepeat")).sendKeys("mat@3775");
+		//driver.findElement(By.id("registerButton")).click();
+		
+	}
+	
+	
+	
+	
 	// ingreso de usuario registrado
-	@Test(description= "CP01 - Ingresar Usuario Existente", priority=1)
+	@Test(description= "CP01 - Ingresar Usuario Existente", priority=2)
 	public void IngresoUsuario(){
 		driver.findElement(By.id("headerProfile")).click();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("signInEmail")));
 		driver.findElement(By.id("signInEmail")).sendKeys("autom3775@gmail.com");
 		driver.findElement(By.cssSelector("#signInPassword")).sendKeys("mat@3775");
-		driver.findElement(By.cssSelector("#signInButton")).click();
-		
-		
+		driver.findElement(By.cssSelector("#registerPasswordRepeat")).click();
+		}
+	
+	@Test
+	public void BuscarArticulo() {
+		driver.findElement(By.xpath( "//a[contains(@class,'headerOption headerOptionMenuOpen')]")).click();
 		
 		
 	}
